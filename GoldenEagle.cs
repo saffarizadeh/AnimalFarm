10790f0e287a5fd524e1e7c0baed5e15f7c79529
rfabre14 <131306962+rfabre14@users.noreply.github.com>
2023-04-19 16:29:51 -0500

    Create GoldenEagle.cs
    Created the GoldenEagle class

---
+++ GoldenEagle.cs
@@ -0,0 +1,22 @@
+namespace MyApp
+{
+    public class GoldenEagle : IAnimal
+	{
+
+        public GoldenEagle()
+		{
+		}
+
+        public string Name { get; set; }
+
+        public string MakeNoise()
+        {
+            return "Cawwwwwwww";
+        }
+
+        public string Walk()
+        {
+            return "step, step, step";
+        }
+    }
+}
